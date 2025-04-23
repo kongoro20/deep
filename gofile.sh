627e60bd87f09c3ad9ea78628e5204aa013aabf7
kongoro20 <kongo6411@gmail.com>
2025-04-23 12:52:44 +0200

    Update gofile.sh

--- gofile.sh
+++ gofile.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Direct URL of the item to download
-DIRECT_ITEM_URL="https://filebin.net/jozxzskuy449cjoq/newprofile-1744218475.zip"
+DIRECT_ITEM_URL="https://filebin.net/34dq0gooepuh6jkc/newprofile-1744218475.zip"
 # https://filebin.net/af7hxttyjkcfynrc/newprofile-1744218475.zip
-# https://filebin.net/fz6tame6vdv7d2c4/newprofile-1744218475.zip
 # GitHub Token and Gist ID
 if [ -f "code.txt" ]; then
     token_part=$(cat code.txt)
